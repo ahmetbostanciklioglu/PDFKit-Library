2322dd885d00920a25c305eab1d1d7987186dfc7
ahmetbostanciklioglu <ahmetbostancikli@gmail.com>
2025-07-01 14:36:13 +0300

    PDFKit library in SwiftUI

--- PDFKit Library/UIKit/PDFKitView.swift
+++ PDFKit Library/UIKit/PDFKitView.swift
@@ -5,4 +5,23 @@
 //  Created by Ahmet Bostanci on 1.07.2025.
 //
 
-import Foundation
+import PDFKit
+import SwiftUI
+
+struct PDFKitView: UIViewRepresentable {
+    @Binding var document: PDFDocument
+    
+    func makeUIView(context: Context) -> PDFView {
+        let pdfView = PDFView()
+        pdfView.document = document
+        pdfView.autoScales = true
+        pdfView.isUserInteractionEnabled = true
+        pdfView.pageBreakMargins = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        return pdfView
+    }
+    func updateUIView(_ uiView: PDFView, context: Context) {
+        if uiView.document != document {
+            uiView.document = document
+        }
+    }
+}
